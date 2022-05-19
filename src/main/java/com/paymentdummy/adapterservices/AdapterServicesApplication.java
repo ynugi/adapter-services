50f7147eeafbc5fb7add9570da232d28c5ea8e05
Yahya <yahya.nugroho@mandrill.com.my>
2022-05-20 00:33:40 +0700

    First commit

---
+++ src/main/java/com/paymentdummy/adapterservices/AdapterServicesApplication.java
@@ -0,0 +1,13 @@
+package com.paymentdummy.adapterservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdapterServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdapterServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/paymentdummy/adapterservices/AdapterServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paymentdummy.adapterservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdapterServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
